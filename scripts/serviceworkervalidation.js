20b9ed152188f87b45ece629419d436e3b1289f9
diogosella <diogosella@outlook.com>
2026-01-05 00:51:31 -0300

    Icon changes

--- scripts/serviceworkervalidation.js
+++
@@ -1,6 +0,0 @@
-
-    if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('Service Worker registrado'))
-    .catch(err => console.error('Erro:', err))
-}
